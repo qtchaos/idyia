eee4c20c087d891640bebb94fb9a75b36596e4f3
chaos <72168435+qtchaos@users.noreply.github.com>
2026-07-22 22:50:42 +0300

    fix: use dynamic import

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,7 @@
-import { DATABASE_AUTH_TOKEN, DATABASE_URL } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
+const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN });
 export const db = drizzle(client, { schema });
